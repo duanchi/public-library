60c90a8f35c84eef86fa464c86b409a4bd651c19
duanchi <07.duanchi@gmail.com>
2015-07-09 00:30:16 +0800

    support Yaconf.

--- CONF.php
+++ CONF.php
@@ -9,31 +9,47 @@
 
 class CONF {
 
-    static private $__engine    =   'Yaconf';
-    static private $__scope     =   'base';
+    const SCOPE_SEPARATOR           =   '_';
+
+
+    static private $__engine        =   'Yaconf';
+    static private $__application   =   NULL;
+    static private $__environment   =   'base';
+
 
     static public function set ($_key, $value) {
 
     }
 
-    static public function get ($_key, $_scope = NULL) {
-        if (NULL == $_scope) {
-            return \Yaconf::get($_key . '-' . $_scope);
-        } else {
-            return \Yaconf::get($_key);
+    static public function get ($_scope, $_key = '', $_environment = NULL, $_application = NULL) {
+
+        NULL    ==  $_application   ?   $_application   =   self::$__application    :   FALSE;
+        ''      ==  $_key           ?   TRUE                                        :   $_key    =   '.' . $_key;
+        NULL    ==  $_environment   ?   $_environment   =   self::$__environment    :   FALSE;
+
+        if (NULL == $_application) {
+            return NULL;
         }
+
+        return \Yaconf::get($_application . self::SCOPE_SEPARATOR . $_scope . self::SCOPE_SEPARATOR . $_environment . $_key);
 
     }
 
-    static public function has ($_key, $_scope = NULL) {
-        if (NULL == $_scope) {
-            return \Yaconf::has($_key . '-' . $_scope);
-        } else {
-            return \Yaconf::has($_key);
+    static public function has ($_scope, $_key = '', $_environment = NULL, $_application = NULL) {
+
+        NULL    ==  $_application   ?   $_application   =   self::$__application    :   FALSE;
+        ''      ==  $_key           ?   TRUE                                        :   $_key    =   '.' . $_key;
+        NULL    ==  $_environment   ?   $_environment   =   self::$__environment    :   FALSE;
+
+        if (NULL == $_application) {
+            return NULL;
         }
+
+        return \Yaconf::has($_application . self::SCOPE_SEPARATOR . $_scope . self::SCOPE_SEPARATOR . $_environment . $_key);
     }
 
-    static public function set_scope ($_scope = 'base') {
-        self::$__scope          =   $_scope;
+    static public function set_environment ($_environment = 'base', $_application = NULL) {
+        self::$__environment        =   $_environment;
+        self::$__application        =   $_application;
     }
 }
