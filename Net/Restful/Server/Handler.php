bc48d0f4a880886d03161bb3a41f7b14ea138a78
duanchi <duanchi@b1703.devel>
2015-08-05 18:38:36 +0800

    08-05

--- Net/Restful/Server/Handler.php
+++ Net/Restful/Server/Handler.php
@@ -11,44 +11,130 @@ namespace Net\Restful\Server;
 
 abstract class Handler
 {
-    private $__request;
-    private $__request_headers;
-    private $__request_body;
-    private $__service;
-    private $__resource;
-    private $__properties;
+    private $__request                      =   [
+        'url'           =>  '',
+        'service'       =>  '',
+        'resource'      =>  '',
+        'parameters'    =>  '',
+        'properties'    =>  [],
+        'headers'       =>  [],
+        'body'          =>  ''
+    ];
 
-    function __construct($_service_regex = '', $_resource_locate = EX_NET_RESTFUL_OPT_RESLOCURL) {}
+    private $__serviceholder                =   NULL;
+    private $__resource_locate              =   EX_NET_RESTFUL_OPT_RESLOCURL;
+    private $__config                       =   [];
 
-    public function GET($_service, $_resource, $_properties) {}
+    function __construct($_url = NULL, $_serviceholder = NULL, $_resource_locate = EX_NET_RESTFUL_OPT_RESLOCURL) {
+        $__tmp_url                          =   $_url ?? $_SERVER['REQUEST_URI'];
+        $__tmp_url                          =   explode('?', $__tmp_url, 2);
 
-    public function DELETE() {}
+        $this->__request                    =   [
+            'url'           =>  $_url,
+            'service'       =>  $__tmp_url[0] ?? '',
+            'resource'      =>  '',
+            'parameters'    =>  $__tmp_url[1] ?? '',
+            'properties'    =>  [],
+            'headers'       =>  [],
+            'body'          =>  ''
+        ];
 
-    public function HEAD() {}
+        $this->__serviceholder              =   $_serviceholder;
+        $this->__resource_locate            =   $_resource_locate;
+        $this->__config                     =   $this->__get_config();
 
-    public function TRACE() {}
+    }
 
-    public function OPTINOS() {}
+    public function GET($_service, $_resource, $_parameters, $_properties) {}
 
-    public function POST() {}
+    public function DELETE($_service, $_resource, $_parameters, $_properties) {}
 
-    public function PUT() {}
+    public function HEAD($_service, $_resource, $_parameters, $_properties) {}
 
-    public function PATCH() {}
+    public function TRACE($_service, $_resource, $_parameters, $_properties) {}
 
-    public function UPDATE() {}
+    public function OPTIONS($_service, $_resource, $_parameters, $_properties) {}
+
+    public function POST($_service, $_resource, $_parameters, $_properties, $_request_body) {}
+
+    public function PUT($_service, $_resource, $_parameters, $_properties, $_request_body) {}
+
+    public function PATCH($_service, $_resource, $_parameters, $_properties, $_request_body) {}
+
+    public function UPDATE($_service, $_resource, $_parameters, $_properties, $_request_body) {}
 
     public function get_request() {
         return $this->__request;
     }
 
-    public function get_request_headers() {
-        return $this->__request_headers;
+    public function handle($_request) {
+
+        /* {{{ PARSE HEADER START */
+
+        /* PARSE HEADER END }}} */
+
+        /* {{{ PARSE PROPERTY START */
+
+        /* PARSE PROPERTY END }}} */
+
+        if (!empty($this->__serviceholder)) {
+            $this->__request['resource']    =   $this->__parse_resource($this->__serviceholder, $this->__resource_locate, $this->__request);
+        }
+
+
+
+        /* {{{ PARSE RESOURCE START */
+
+        /* PARSE RESOURCE END }}} */
     }
 
-    public function handle() {
-        /*{{{ PARSE RESOURCE START*/
+    private function __get_config() {
+        return \CONF::get('restful', NULL, NULL, PUBLIC_LIBRARY_KEY);
+    }
 
-        /*PARSE RESOURCE END}}}*/
+    private function __parse_resource($_serviceholder, $_resource_locate, $_request) {
+
+        $__return_value                     =   '';
+
+        switch ($_resource_locate) {
+            case EX_NET_RESTFUL_OPT_RESLOCPRY:
+                if (is_string($_serviceholder)) {
+                    $__return_value         =   $this->__request['properties'][$_serviceholder]['value'] ?? '';
+                }
+                elseif (isset($_serviceholder['key']) && isset($_serviceholder['regex'])) {
+                    $__tmp_match            =   [];
+                    preg_match($_serviceholder['regex'], $this->__request['properties'][$_serviceholder['key']]['value'], $__tmp_match);
+
+                    $__return_value         =   $__tmp_match[1] ?? '';
+                }
+                elseif (isset($_serviceholder['key'])) {
+                    $__return_value         =   $this->__request['properties'][$_serviceholder['key']]['value'];
+                }
+                break;
+
+            case EX_NET_RESTFUL_OPT_RESLOCHDR:
+                if (!empty($this->__request['headers']) && !empty($_serviceholder)) {
+                    foreach ($this->__request['headers'] as $__header) {
+                        $__tmp_match            =   [];
+                        if (preg_match($_serviceholder, $__header, $__tmp_match)) {
+                            $__return_value     =   $__tmp_match[1] ?? '';
+                            break;
+                        }
+                    }
+                }
+                break;
+
+            case EX_NET_RESTFUL_OPT_RESLOCURL:
+            default:
+                if (!empty($_serviceholder)) {
+                    $__tmp_match                =   [];
+                    if (preg_match($_serviceholder, $this->__request['url'], $__tmp_match)) {
+                        $__return_value         = $__tmp_match[1] ?? '';
+                    }
+                }
+                break;
+        }
+
+        return $__return_value;
     }
 }
